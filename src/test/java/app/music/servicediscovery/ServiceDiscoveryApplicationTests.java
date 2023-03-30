a9af7f66e693e136fb1b32ce8db661815d838e2d
Samandar Akbarov <akbarov.samandar@gmail.com>
2023-03-30 14:38:46 +0500

    Adding service discovery

---
+++ src/test/java/app/music/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package app.music.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
